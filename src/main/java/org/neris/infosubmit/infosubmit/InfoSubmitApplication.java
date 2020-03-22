79c23eec290143c617bcc965e77d358042c3e8cf
hanlei <2551591685@qq.com>
2020-03-22 13:59:11 +0800

    上传

---
+++ src/main/java/org/neris/infosubmit/infosubmit/InfoSubmitApplication.java
@@ -0,0 +1,13 @@
+package org.neris.infosubmit.infosubmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoSubmitApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(InfoSubmitApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/neris/infosubmit/infosubmit/InfoSubmitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neris.infosubmit.infosubmit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoSubmitApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
